9cf4d54f8a8c00c405a26e2736af3431fb3d2cbf
shadyxv <shovethatshitin@gmail.com>
2024-04-15 19:12:12 +0545

    add store

--- app/src/routes/__root.tsx
+++ app/src/routes/__root.tsx
@@ -1,6 +1,31 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
+import { useStore } from '@tanstack/react-store';
+import { Store } from '@tanstack/store';
+
+export const store = new Store({
+  dogs: 0,
+  cats: 0,
+});
+
+const Display = ({ animal }) => {
+  const count = useStore(store, state => state[animal]);
+  return <div>{`${animal}: ${count}`}</div>;
+};
+
+const updateState = animal => {
+  store.setState(state => {
+    return {
+      ...state,
+      [animal]: state[animal] + 1,
+    };
+  });
+};
+const Increment = ({ animal }) => (
+  <button onClick={() => updateState(animal)}>My Friend Likes {animal}</button>
+);
+
 export const Route = createRootRoute({
   component: () => (
     <>
@@ -13,6 +38,12 @@ export const Route = createRootRoute({
         </Link>
       </div>
       <hr />
+      <h1>How many of your friends like cats or dogs?</h1>
+      <p>Press one of the buttons to add a counter of how many of your friends like cats or dogs</p>
+      <Increment animal="dogs" />
+      <Display animal="dogs" />
+      <Increment animal="cats" />
+      <Display animal="cats" />
       <Outlet />
       <TanStackRouterDevtools />
     </>
